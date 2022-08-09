944c2e22a185fc833db5928fed5ebdb27e66dc28
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-08-08 23:38:27 -0700

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-fetch("pokemon.json")
-    .then(response => response.json())
-    .then(data => {
-    let pokemonValues = data["Generation one"];
-    console.log(pokemonValues);
-    //let text = document.getElementById('text').innerHTML = pokemonValues[0]['jap']
-    //let img:HTMLImageElement = document.getElementById("img")!.src = pokemonValues[1]['image'];
-});
