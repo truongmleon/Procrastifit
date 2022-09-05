582df3e21fa99941430d29e3d498814eeae3e287
Leon <junzai12@icloud.com>
2022-09-05 15:43:45 -0700

    package changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        app: "src/backend/views/index.html"
-      }
-    }
-  }
+  plugins: [react()]
 })
