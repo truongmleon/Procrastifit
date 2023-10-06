7b6d22612598a3f8a3e0da6c1da0615dbfe536df
Leon <junzai12@icloud.com>
2023-10-06 12:07:20 -0700

    Update Service.tsx

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -48,8 +48,8 @@ const Service = () => {
 
             <h2 data-aos="zoom-in-right" id="error-title">See an error with the site? 🫡</h2>
             <p data-aos="zoom-in-right" id="error-paragraph">I had a lot of fun coding this website!1! However, mistakes are bound to occur. 
-            Free feel to contribute 🫵 to it by letting me (Leon Truong) know or by making a <a href="https://github.com/truongmleon/Key-Club-Website/tree/main" target="_blank">pull request</a>. 
-            This is an open source project under the <a href="https://github.com/truongmleon/Key-Club-Website/blob/main/LICENSE" target="_blank">MIT License</a>!</p>
+            Free feel to contribute 🫵 to it by letting me (Leon Truong) know or by making a <a href="https://github.com/truongmleon/National-Honors-Society-Site" target="_blank">pull request</a>. 
+            This is an open source project under the <a href="https://github.com/truongmleon/National-Honors-Society-Site/blob/main/LICENSE" target="_blank">MIT License</a>!</p>
             
         </div>
     )
