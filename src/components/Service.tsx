0b53316b0c1c7a59b24e757e0ac6016cfe5da88e
Leon <junzai12@icloud.com>
2023-10-05 23:11:05 -0700

    tutoring service

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -21,7 +21,12 @@ const Service = () => {
             </ul> 
             <ul className="event-list">
                 <li className="event-title">Ongoing Events :3</li>
-                <li>Holiday Teacher Card Making + Gingerbread House Raffle</li>
+                <li> 
+                    <a href="https://upchieve.org/volunteer-with-us" target="_blank">UPchieve</a> (Indefinite)
+                    <li className="event-info">Tutor from elementary English/math/science to the high school level (I enjoy tutoring AP Calculus!)</li>
+                    <li className="event-info">Students who use UPchieve are typically economically disadvantaged students; you'll make a large impact!</li>
+                    <li className="event-info">Consider that a significant part of Kentwood's student body receives Free/Reduced Lunch</li>
+                </li>
                 <li>Book Drive for KCLS</li>
             </ul> 
 
