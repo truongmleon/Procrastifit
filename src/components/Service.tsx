7f3ef45320bfab31f14d356e4298def352e0425f
Leon <junzai12@icloud.com>
2023-10-11 14:30:18 -0700

    service font changes

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -22,14 +22,14 @@ const Service = () => {
             <ul className="event-list">
                 <li className="event-title" data-aos="fade-right">Ongoing Events :3</li>
                 <li> 
-                    <b data-aos="fade-left"><a className="event" href="https://upchieve.org/volunteer-with-us" target="_blank">UPchieve</a> (Indefinite)</b>
+                    <b data-aos="fade-left" className="event-name"><a className="event" href="https://upchieve.org/volunteer-with-us" target="_blank">UPchieve</a> (Indefinite)</b>
                     <li className="event-info" data-aos="fade-right">Tutor from elementary English/math/science to the high school level (I enjoy tutoring AP Calculus!)</li>
                     <li className="event-info" data-aos="fade-left">Students who use UPchieve are typically economically disadvantaged students; you'll make a large impact!</li>
                     <li className="event-info" data-aos="fade-right">Consider that a significant part of Kentwood's student body receives Free/Reduced Lunch</li>
                 </li>
 
                 <li>
-                    <b data-aos="fade-left"><a className="event" href="https://play.freerice.com/profile-login" target="_blank">FREERICE</a> (Indefinite)</b>
+                    <b data-aos="fade-left" className="event-name"><a className="event" href="https://play.freerice.com/profile-login" target="_blank">FREERICE</a> (Indefinite)</b>
                     <li className="event-info" data-aos="fade-right">I suggest creating an account to keep track of your progress</li> 
                     <li className="event-info" data-aos="fade-left">For every question answered correctly, 10 grams of rice is donated for the World Food Programme</li>
                     <li className="event-info" data-aos="fade-right">Questions include science, math, humanities, and foreign language</li>
@@ -38,7 +38,7 @@ const Service = () => {
                 </li>
 
                 <li>
-                    <b data-aos="fade-left"><a className="event" href="https://www.bemyeyes.com/" target="_blank">Be My Eyes</a> (Indefinite)</b>
+                    <b data-aos="fade-left" className="event-name"><a className="event" href="https://www.bemyeyes.com/" target="_blank">Be My Eyes</a> (Indefinite)</b>
                     <li className="event-info" data-aos="fade-left">A good chunk of people have vision problems and would gladly need the extra help</li>
                     <li className="event-info" data-aos="fade-right">You will get in a video call assisting the visually impaired with everyday tasks</li>
                     <li className="event-info" data-aos="fade-left"><a href="https://apps.apple.com/us/app/be-my-eyes/id905177575?pt=15492800&ct=BeMyEyesWebsite&mt=8" target="_blank">App Store</a></li>
