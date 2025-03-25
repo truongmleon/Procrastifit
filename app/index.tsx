928a9109a76810962e4f794cf86c150b81237941
Leon <junzai12@icloud.com>
2025-03-24 17:39:52 -0700

    login stuff

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
-import { Image, StyleSheet, Platform, View, StatusBar } from 'react-native';
-
+import { Image, StyleSheet, TextInput, View, StatusBar } from 'react-native';
+import React, { useState } from 'react';
 import * as AppleAuthentication from 'expo-apple-authentication';
 import { ThemedText } from '@/components/ThemedText';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <View style={styles.container}>
@@ -12,9 +14,23 @@ export default function App() {
         source={require('../assets/images/logo.png')} 
         style={styles.logo}
       />
-      <ThemedText style={{ fontSize: 20, marginBottom: 20 }}>
-        SignWelcome
-      </ThemedText>
+      <TextInput
+        style={styles.input}
+        autoComplete="email"
+        placeholder='Email Address'
+        placeholderTextColor="#A9A9A9"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+      secureTextEntry
+        style={styles.input}
+        autoComplete="password"
+        placeholder='Password'
+        placeholderTextColor="#A9A9A9"
+        value={password}
+        onChangeText={setPassword}
+      />
       <AppleAuthentication.AppleAuthenticationButton
         buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
         buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
@@ -51,6 +67,13 @@ const styles = StyleSheet.create({
     height: "10%",
     resizeMode: 'contain',
     transform: [{ translateY: "-270%" }],
+  },
+  input: {
+    height: 40,
+    width: "70%",
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
   container: {
     flex: 1,
